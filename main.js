bae64ad47c89061ebebb46c59e330fbff5584353
MirandaMcCoy <mirandamccoy5@gmail.com>
2017-12-11 01:34:06 -0600

    Added menu bar options and add item window

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 app.on('ready', () => {
     let win = new BrowserWindow({width:800, height: 600});
-    win.loadURL('file://${__dirname}/index.html')
-})
+    win.loadURL('file://' + __dirname + '/index.html');
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+
+    Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow(){
+    let win = new BrowserWindow({width:400, height: 200});
+    win.loadURL('file://' + __dirname + '/add.html');
+}
+
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items'
+            },
+            {
+                label: 'Quit',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Developer Tools'
+    }
+]
